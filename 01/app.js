9cd7edfe0fd53b1ec4be884e4106541038525b6d
marrcelp <marcel.piaszczyk@gmail.com>
2024-12-10 17:39:28 +0100

    resolved exercise 1

--- 01/app.js
+++ 01/app.js
@@ -1,1 +1,12 @@
 console.log('DOM');
+
+const element = document.querySelector('.comments__item.comments__item--newest');
+
+if (element){
+    const elementsWithAtribute = element.querySelectorAll('[data-info]');
+    console.log(elementsWithAtribute);
+    console.log(`Znaleziono ${elementsWithAtribute.length} elementy z podanym atrybutem`);
+    
+} else {
+    console.log(`Nie znaleziono elementu o podanej klasie`);
+}
