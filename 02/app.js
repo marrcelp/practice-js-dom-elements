eefeec1e5ee86562f49eb980c7dd41bb9a3c8238
marrcelp <marcel.piaszczyk@gmail.com>
2024-12-10 18:08:22 +0100

    resolved exercise 2

--- 02/app.js
+++ 02/app.js
@@ -1,1 +1,10 @@
 console.log('DOM');
+
+const linksWithUrl = document.querySelectorAll('[data-url]');
+console.log(linksWithUrl);
+
+
+linksWithUrl.forEach((element) => {
+    const elementUrl = element.getAttribute('data-url');
+    element.setAttribute('href', elementUrl)
+})
